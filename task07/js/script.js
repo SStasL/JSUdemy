573a1342773072fa53dff05ee0da6cda579414c4
SStasL <stas.shmulich@gmail.com>
2019-10-02 17:50:53 +0300

    add task 07

---
+++ task07/js/script.js
@@ -0,0 +1,22 @@
+let budgetValue = document.getElementsByClassName("budget-value"),
+    daybudgetValue = document.getElementsByClassName("daybudget-value"),
+    levelValue = document.getElementsByClassName("level-value"),
+    expensesValue = document.getElementsByClassName("expenses-value"),
+    optionalexpensesValue = document.getElementsByClassName("optionalexpenses-value"),
+    incomeValue = document.getElementsByClassName("income-value"),
+    monthsavingsValue = document.getElementsByClassName("monthsavings-value"),
+    yearsavingsValue = document.getElementsByClassName("yearsavings-value"),
+    expensesItem = document.getElementsByClassName("expenses-item"),
+    buttons = document.getElementsByTagName("button"),
+    submitButton = buttons[0],
+    calculateButton = buttons[1],
+    inputFields = document.querySelectorAll(".optionalexpenses-item"),
+    chooseIncome = document.querySelectorAll(".choose-income"),
+    savings = document.querySelectorAll("#savings");
+
+    console.log(savings);
+
+    
+
+
+    
