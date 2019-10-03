90127c1fa4c24c193c06bc6ba24ac1905e7cb83f
SStasL <stas.shmulich@gmail.com>
2019-10-03 11:29:14 +0300

    add exec task 07

--- 20/script.js
+++ 20/script.js
@@ -2,7 +2,9 @@ let box = document.getElementById("box"),
     btn = document.getElementsByTagName("button"),
     circle = document.getElementsByClassName("circle"),
     heart = document.querySelectorAll(".heart"),
-    oneHeart = document.querySelector(".heart");
+    oneHeart = document.querySelector(".heart"),
+    txt = document.querySelector(".txt"),
+    wrapper = document.querySelector(".wrapper");
 
 console.log(box);
 console.log(btn);
@@ -10,3 +12,44 @@ console.log(btn[0]);
 console.log(circle[2]);
 console.log(heart[1]);
 console.log(oneHeart);
+txt.style.fontSize = "30px";
+console.log(txt);
+
+box.style.backgroundColor = "blue";
+
+btn[1].style.borderRadius = "100%";
+
+circle[0].style.backgroundColor = "red";
+circle[1].style.backgroundColor = "yellow";
+circle[2].style.backgroundColor = "green";
+
+// for (let i = 0; i < heart.length; i++) {
+//     heart[i].style.backgroundColor = "blue";
+// }
+
+// heart.forEach(function(item, i, hearts) {
+//     item.style.backgroundColor = "blue";
+// })
+
+let div = document.createElement("div"),
+    text = document.createTextNode("Here");
+
+div.classList.add("black");
+
+// document.body.appendChild(div);
+// wrapper.appendChild(div);
+
+// div.innerHTML = "<h1>Hello world!</h1>";
+div.textContent = "Hello world!";
+
+document.body.insertBefore(div, circle[0]);
+
+document.body.removeChild(circle[1]);
+
+wrapper.removeChild(heart[1]);
+
+
+document.body.replaceChild(btn[1], circle[1]);
+
+console.log(div);
+console.log(text);
--- task07/js/script.js
+++ task07/js/script.js
@@ -9,14 +9,44 @@ let budgetValue = document.getElementsByClassName("budget-value"),
     expensesItem = document.getElementsByClassName("expenses-item"),
     buttons = document.getElementsByTagName("button"),
     submitButton = buttons[0],
-    calculateButton = buttons[1],
+    submitOptionButton = buttons[1],
+    calculateButton = buttons[2],
     inputFields = document.querySelectorAll(".optionalexpenses-item"),
     chooseIncome = document.querySelectorAll(".choose-income"),
-    savings = document.querySelectorAll("#savings");
+    sum = document.querySelectorAll("#sum"),
+    percent = document.querySelectorAll("#percent"),
+    year = document.querySelector(".year-value"),
+    month = document.querySelector(".month-value"),
+    day = document.querySelector(".day-value"),
+    divClassApp = document.querySelectorAll(".app")[0],
+    divClassMain = document.querySelectorAll(".main")[0],
+    divClassData = document.querySelectorAll(".data")[0];
 
-    console.log(savings);
+let sumTmp = [];
+sumTmp[0] = sum[0];
+sumTmp[0].innerHTML = 9999;
+
+// divClassData.replaceChild(sum[0], sumTmp[0]);
+
+
+console.log(savings);
+console.log(submitButton);
+console.log(calculateButton);
+console.log(inputFields[0]);
+console.log(inputFields[1]);
+console.log(inputFields[2]);
+console.log(chooseIncome[0]);
+console.log(savings[0]);
+console.log(sum[0]);
+console.log(percent[0]);
+console.log(year);
+console.log(month);
+console.log(day);
 
     
 
 
-    
+console.log(divClassMain);
+console.log(divClassData);
+// document.body.divClassApp.divClassMain.divClassData.replaceChild(sum[0], sumTmp[0]);
+divClassData.replaceChild(sum[0], sumTmp[0]);
