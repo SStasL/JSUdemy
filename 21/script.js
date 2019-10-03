b7619303869b157203791cf442d025d7255cb304
SStasL <stas.shmulich@gmail.com>
2019-10-03 16:21:20 +0300

    add lesson 21

---
+++ 21/script.js
@@ -0,0 +1,40 @@
+let btn = document.querySelectorAll("button");
+let wrap = document.querySelector(".wrapper");
+let link = document.querySelector("a");
+
+// btn[0].onclick = function() {
+//     alert("Вы нажали первую кнопку");
+// };
+
+// btn[0].onclick = function() {
+//     alert("Вы опять нажали первую кнопку");
+// };
+
+// btn[0].addEventListener("click", function(event) {
+//     // console.log(event);
+//     // let target = event.target;
+//     // target.style.display = "none";
+//     console.log("Произошло событие: " + event.type + " На элементе: " + event.target);
+// });
+
+wrap.addEventListener("click", function() {
+    console.log("Произошло событие: " + event.type + " На элементе: " + event.target);
+});
+
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Произошло событие: " + event.type + " На элементе: " + event.target);
+});
+
+btn.forEach(function(item) {
+    item.addEventListener("mouseleave", function() {
+        console.log("ВышлиЙ!")
+    });
+});
+
+
+
+
+// btn[0].addEventListener("mouseenter", function() {
+//     alert("Вы навели на первую кнопку");
+// });
