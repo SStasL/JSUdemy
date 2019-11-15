6fa892d8b2989cb9710a44e5d91c68d21aea972e
SStasL <stas.shmulich@gmail.com>
2019-11-15 16:07:34 +0200

    task 14 with new currency

--- task14/currency/js/script.js
+++ task14/currency/js/script.js
@@ -1,59 +1,40 @@
+
 let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
+inputRub.addEventListener('input', () => {
 
-function exec(elem) {
-    elem.addEventListener("input", function(event) {
-        function reqData() {
-            return new Promise(function(resolve, reject) {
-                let request = new XMLHttpRequest();
+    function catchData() {
 
-                request.open('GET', 'js/current.json');
-                request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
-                request.onreadystatechange = function() {
-                    if (request.readyState === 4 && request.status == 200) {
-                        resolve()
-                    } else {
-                        reject()
-                    }
+        return new Promise(function(resolve, reject){
+            let request = new XMLHttpRequest();
+            request.open("GET", "js/current.json");
+        
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
+        
+            request.onload = function() {
+                if(request.readyState === 4) {
+                        if(request.status == 200) {
+                            resolve(this.response)
+                        }
+                        else {
+                            reject();
+                        
+                        }
                 }
+            }
+        });
+    };
 
-                request.send();
-            })
-        }
-
-        function clearInput() {
-            inputRub = inputRub.innerHTML = "";
-        }
-
-        reqData()
-            .then(() => {
-                let data = JSON.parse(request.response);
-                inputUsd.value = inputRub.value / data.usd;
-            })
-            .catch(() => inputUsd.value = "Что-то пошло не так!")
-            .then(clearInput)
+    catchData()
+    .then(response => {
+        console.log(response);
+        let data = JSON.parse(response);
+        inputUsd.value = inputRub.value / data.usd;
     })
-}
+    .then(() => console.log(5000))
+    .catch(() => inputUsd.value = "Что-то пошло не так")
 
-exec(inputRub);
 
-// inputRub.addEventListener('input', () => {
-//     let request = new XMLHttpRequest();
-
-//     request.open('GET', 'js/current.json');
-//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-//     request.send();
-
-//     request.addEventListener('readystatechange', function() {
-//         if (request.readyState === 4 && request.status == 200) {
-//             let data = JSON.parse(request.response);
-
-//             inputUsd.value = inputRub.value / data.usd;
-//         } else {
-//             inputUsd.value = "Что-то пошло не так!";
-//         }
-//     });
-
-// });
+});
